19dd9b70a2adc6cc74615c43029fbd170d17ef57
barblazen <barbaramasaltseva@gmail.com>
2024-11-16 12:34:14 +0100

    Initial commit

--- lesson 6/homework/3.py
+++ lesson 6/homework/3.py
@@ -2,3 +2,10 @@
 #Пользователь вводит строку. Напишите программу, которая подсчитает и выведет количество букв и цифр в этой строке.
 # Например, для строки "Привет123" результатом должно быть "Букв: 6, Цифр: 3".
 string = input("Enter a string of letters and numbers : ")
+letters = sum(1 for char in string if char.isalpha())
+digits = sum(1 for char in string if char.isdigit())
+# digits = 0
+# for char in string:
+#     if char.isdigit():
+#         digits+= 1
+print(f"Букв : {letters}, Цифр : {digits}")
